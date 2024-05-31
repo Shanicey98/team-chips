624e748d50909ecb5c88e6d4db262cc02caa6d40
myathetchai <myathetchai441@gmail.com>
2024-05-31 09:30:03 +0800

    Co-authored-by: Shanicey98 <Shanicey98@users.noreply.github.com> Co-authored-by: purp <prppy@users.noreply.github.com>

--- App.js
+++ App.js
@@ -2,7 +2,8 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import Map from './screens/Map'
+import Map from './screens/Map';
+import Home from './screens/Home'
 
 const Stack = createNativeStackNavigator();
 
@@ -12,8 +13,10 @@ export default function App() {
   return (
 
   <NavigationContainer>
-  <Stack.Navigator initialRouteName='Map'>
+  <Stack.Navigator initialRouteName='map'>
   <Stack.Screen name = "map" component = {Map} />
+  <Stack.Screen name = "Home" component= {Home} />
+
 
     
   </Stack.Navigator>
